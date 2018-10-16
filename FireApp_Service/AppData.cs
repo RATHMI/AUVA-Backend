dcfa404051c9cf9cf6b3f763c3de0903bc054007
Michael Rath <40146720140057@litec.ac.at>
2018-10-16 16:51:12 +0200

    161020181651

--- FireApp_Service/AppData.cs
+++ FireApp_Service/AppData.cs
@@ -10,8 +10,24 @@ using System.Xml.Serialization;
 
 namespace FireApp.Service {
     public static class AppData {
+
         static AppData() {
             BsonMapper.Global.Entity<User>()
+                .Id(x => x.Id, true);
+
+            BsonMapper.Global.Entity<Machine>()
+                .Id(x => x.Id, true);
+
+            BsonMapper.Global.Entity<MachineType>()
+                .Id(x => x.Id, true);
+
+            BsonMapper.Global.Entity<Room>()
+                .Id(x => x.Id, true);
+
+            BsonMapper.Global.Entity<SecurityClothes>()
+                .Id(x => x.Id, true);
+
+            BsonMapper.Global.Entity<Warning>()
                 .Id(x => x.Id, true);
         }
 
@@ -21,6 +37,61 @@ namespace FireApp.Service {
         }
         public static LiteCollection<User> UserTable(this LiteDatabase db) {
             return db.GetCollection<User>("user");
+        }
+        #endregion
+
+        #region MachineDB
+        public static LiteDatabase MachineDB()
+        {
+            return new LiteDatabase(AppSettings.AuvaDBPath);
+        }
+        public static LiteCollection<Machine> MachineTable(this LiteDatabase db)
+        {
+            return db.GetCollection<Machine>("machine");
+        }
+        #endregion
+
+        #region MachineTypeDB
+        public static LiteDatabase MachineTypeDB()
+        {
+            return new LiteDatabase(AppSettings.AuvaDBPath);
+        }
+        public static LiteCollection<MachineType> MachineTypeTable(this LiteDatabase db)
+        {
+            return db.GetCollection<MachineType>("machineType");
+        }
+        #endregion
+
+        #region RoomDB
+        public static LiteDatabase RoomDB()
+        {
+            return new LiteDatabase(AppSettings.AuvaDBPath);
+        }
+        public static LiteCollection<Room> RoomTable(this LiteDatabase db)
+        {
+            return db.GetCollection<Room>("room");
+        }
+        #endregion
+
+        #region SecurityClothesDB
+        public static LiteDatabase SecurityClothesDB()
+        {
+            return new LiteDatabase(AppSettings.AuvaDBPath);
+        }
+        public static LiteCollection<SecurityClothes> SecurityClothesTable(this LiteDatabase db)
+        {
+            return db.GetCollection<SecurityClothes>("securityClothes");
+        }
+        #endregion
+
+        #region WarningDB
+        public static LiteDatabase WarningDB()
+        {
+            return new LiteDatabase(AppSettings.AuvaDBPath);
+        }
+        public static LiteCollection<Warning> WarningTable(this LiteDatabase db)
+        {
+            return db.GetCollection<Warning>("warning");
         }
         #endregion
     }
