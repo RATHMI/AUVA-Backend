29e73a6ba635e0b4bf2048a0febb8d6e6c38bf3c
Michael Rath <40146720140057@litec.ac.at>
2018-11-13 14:33:34 +0100

    13.11.2018 14:33

--- FireApp_Service/Controllers/MachineController.cs
+++ FireApp_Service/Controllers/MachineController.cs
@@ -8,7 +8,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace FireApp.Service.Controllers {
+namespace AUVA.Service.Controllers {
     public class MachineController : ApiController
     {
         /// <summary>
--- FireApp_Service/DatabaseOperations/Machines.cs
+++ FireApp_Service/DatabaseOperations/Machines.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace FireApp.Service.DatabaseOperations
+namespace AUVA.Service.DatabaseOperations
 {
     public static class Machines
     {
@@ -10,7 +10,7 @@ namespace FireApp.Service.DatabaseOperations
         {
             try
             {
-                return AUVA.Service.DatabaseOperations.LiteDB.LiteDbUpserts.UpsertMachine(machine);
+                return LiteDB.LiteDbUpserts.UpsertMachine(machine);
             }
             catch (Exception ex)
             {
@@ -26,7 +26,7 @@ namespace FireApp.Service.DatabaseOperations
         /// <returns>Returns true if the machine was deleted.</returns>
         public static bool Delete(int machineId)
         {
-            return AUVA.Service.DatabaseOperations.LiteDB.LiteDbDeletes.DeleteMachine(machineId);
+            return LiteDB.LiteDbDeletes.DeleteMachine(machineId);
         }
 
         /// <summary>
@@ -36,12 +36,12 @@ namespace FireApp.Service.DatabaseOperations
         /// <returns>Returns a machine with a matching machineId.</returns>
         public static Machine GetById(int machineId)
         {
-            return AUVA.Service.DatabaseOperations.LiteDB.LiteDbQueries.QueryMachine(machineId);
+            return LiteDB.LiteDbQueries.QueryMachine(machineId);
         }
 
         public static IEnumerable<Machine> GetAll()
         {
-            return AUVA.Service.DatabaseOperations.LiteDB.LiteDbQueries.QueryMachines();
+            return LiteDB.LiteDbQueries.QueryMachines();
         }
     }
 }
